519f54bf84c1e84f95aa4bd46616959c68b06140
Romulo Negrillo Jr <romnegrillo@gmail.com>
2021-06-27 05:11:27 +0800

    File storage setup to AWS S3.

--- users/models.py
+++ users/models.py
@@ -9,12 +9,12 @@ class Profile(models.Model):
     def __str__(self):
         return "{}'s Profile".format(self.user.username)
 
-    def save(self, *args, **kwargs):
-        super().save()
+    # def save(self, *args, **kwargs):
+    #     super().save()
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
